3a394e638aad2af3853ad1a7552ba416bac8d0fc
emirhero <emirhero@gmail.com>
2019-03-13 19:28:42 +0100

    Deletes FirebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyD4DiF5vwX7VRlJ24hjXkRSsn1AQDfFyHQ",
-    authDomain: "glavugore-858fc.firebaseapp.com",
-    databaseURL: "https://glavugore-858fc.firebaseio.com",
-    projectId: "glavugore-858fc",
-    storageBucket: "glavugore-858fc.appspot.com",
-    messagingSenderId: "15380995183"
-  };
- 
